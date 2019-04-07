c9dcaddf81728e7c1b22fe82437465e76c245d08
ktekelis <47675763+ktekelis@users.noreply.github.com>
2019-04-07 15:38:45 -0400

    Add files via upload

--- PrimeTest.java
+++ PrimeTest.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 /**
  * This program tests whether an integer is a prime number.
- * 
- * @author Konstantinos Tekelis 
+ *
+ * @author Konstantinos Tekelis
  * @version 2/23/15
  */
 public class PrimeTest {
@@ -15,21 +15,19 @@ public class PrimeTest {
         do {
             System.out.print("Please enter a positive integer: ");
             num = Integer.parseInt(console.nextLine() );
-            boolean flag = true;
+            boolean number = true;
             for (int i = 2; i <= (int) Math.sqrt(num); i++ ) {
-                 if (num % i == 0) { // is i a factor or num?
-                     flag = false;  // num is not prime
+                 if (num % i == 0) number = false;
                      break;
                 }
             }
-        
-            if (flag) 
+
+            if (number)
                System.out.println(num + " is prime");
-            else 
+            else
                System.out.println(num + " is not prime");
-            System.out.print("Do you want to try again? please " + 
-                "enter Y for yes and N for no: ");
-            response = console.nextLine(); 
+               System.out.print("Do you want to try again? please " +  "enter Y for yes and N for no: ");
+            response = console.nextLine();
         } while (response.charAt(0) == 'Y' || response.charAt(0) == 'y');
         System.out.println("Goodbye");
     }
